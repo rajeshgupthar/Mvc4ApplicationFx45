5202403ca420bd0186e6f42f570361d2d3a53c71
davidebbo <david.ebbo@microsoft.com>
2013-02-10 07:44:14 -0800

    Test handler

---
+++ Mvc4Application/foo.ashx
@@ -0,0 +1,1 @@
+﻿<%@ WebHandler Language="C#" CodeBehind="foo.ashx.cs" Class="Mvc4Application.foo" %>
---
+++ Mvc4Application/foo.ashx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Mvc4Application
+{
+    /// <summary>
+    /// Summary description for foo
+    /// </summary>
+    public class foo : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.ContentType = "text/plain";
+            context.Response.Write("Hello World");
+
+            throw new Exception("zzz");
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
