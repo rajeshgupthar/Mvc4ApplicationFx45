d8011c0805709904fdba530bc634a2607c2ae6a1
davidebbo <david.ebbo@microsoft.com>
2014-01-20 16:02:35 -0800

    222

--- Mvc4Application/Controllers/HomeController.cs
+++ Mvc4Application/Controllers/HomeController.cs
@@ -11,7 +11,7 @@ namespace Mvc4Application.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application. 222";
 
             Trace.TraceInformation("Sample info trace");
             Trace.TraceWarning("Sample warning trace");
